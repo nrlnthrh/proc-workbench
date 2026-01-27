dd2978534fbe5b463f3abe220db0126978a27557
nrlnthrh <norleen39@gmail.com>
2026-01-27 15:37:05 +0800

    changes made

--- main.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-# The cloud start streamlit using the correct port
-subprocess.run([
-    "streamlit", "run", "app.py", 
-    "--server.port", "8080", 
-    "--server.address", "0.0.0.0"
-])
